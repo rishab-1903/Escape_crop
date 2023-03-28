52ebab2eb945d62869c838ec7e78579672f3348f
rishab-1903 <128292780+rishab-1903@users.noreply.github.com>
2023-03-28 14:54:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import streamlit as st
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 
+st.set_page_config(page_title="Demo Labtest")
+
+url = 'http://localhost:5173/'
+if st.button('Go to Home Page'):
+    st.markdown(f'<a href="{url}" target="_blank">Click here to go to home page</a>', unsafe_allow_html=True)
 
 def book_lab_test():
     st.title("Book a Lab Test")
-
-    # Add input fields for user information
     name = st.text_input("Name")
     phone_number = st.text_input("Phone Number")
     address = st.text_area("Address")
-    state = st.selectbox("State", ['States','Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
-)
+    state = st.selectbox("State",
+                         ['States', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa',
+                          'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jharkhand', 'Karnataka', 'Kerala',
+                          'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha',
+                          'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh',
+                          'Uttarakhand', 'West Bengal']
+                         )
     borewell_connection = st.radio("Do you have a borewell connection?", options=["Yes", "No"])
-
 
     if st.button("Submit Lab Test Request"):
 
@@ -28,22 +35,13 @@ def book_lab_test():
         elif borewell_connection == "":
             st.error("Please select whether you have a borewell connection or not")
         else:
-            # Send lab test request to backend and show success message
             st.success("Lab test request submitted successfully. We will contact you soon.")
-
-
-# if __name__ == "__main__":
 
 crop_data = pd.read_csv("Crop_recommendation.csv")
 
-st.set_page_config(page_title="Demo Labtest")
-
-
 def knn_predictor(n, p, k, temperature, humidity, ph, rainfall):
-
     X = crop_data.iloc[:, :-1]
     y = crop_data.iloc[:, -1]
-
 
     knn = KNeighborsClassifier(n_neighbors=5)
 
@@ -53,8 +51,8 @@ def knn_predictor(n, p, k, temperature, humidity, ph, rainfall):
 
     return prediction[0]
 
-def app():
 
+def app():
     st.title("Demo Labtest")
 
     # Add input fields for the model
@@ -66,11 +64,9 @@ def app():
     ph = st.number_input("pH value", value=0.0)
     rainfall = st.number_input("Rainfall (in mm)", value=0.0)
 
-
     if st.button("Get Crop Recommendation"):
         prediction = knn_predictor(n, p, k, temperature, humidity, ph, rainfall)
-        st.success(f"Recommended crop: {prediction}")
-
+        st.success(f"Recommended crop: {prediction}")\
 
 if __name__ == "__main__":
     book_lab_test()
